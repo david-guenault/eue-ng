8c0a9a040f19cd89cbd941d788a2c1b81dc682ea
David GUENAULT <dguenault@monitoring-fr.org>
2013-09-07 11:45:13 +0200

    renamed class modules

---
+++ lib/eueauth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+
+
+class auth:
+
+    def __init__(self):
+        return
+
+    def check_auth(self, user, password):
+        """
+        Verify authentication credentials
+        """
+        return True
---
+++ lib/euedb.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+
+import os
+import sys
+import time
+import re
+import MySQLdb
+from MySQLdb import cursors
+
+
+class mysql:
+
+    cn = None
+
+    host = None
+    port = None
+    user = None
+    passw = None
+
+    def __init__(self, host, user, passw, db, port=3306):
+        self.port = port
+        self.host = host
+        self.user = user
+        self.passw = passw
+        self.db = db
+        if not self.connect():
+            return False
+
+    def connect(self):
+        """
+        connect to the mysql db
+        """
+        try:
+            self.cn = MySQLdb.connect(
+                host=self.host, user=self.user, passwd=self.passw,
+                db=self.db, port=self.port)
+            return True
+        except:
+            return False
+
+    def querySelect(self, query):
+        """
+        do the select query and return the result as an array of dictionaries
+        """
+        data = []
+        cur = self.cn.cursor(cursors.DictCursor)
+        try:
+            cur.execute(query)
+        except:
+            return False
+
+        data = cur.fetchall()
+        cur.close()
+        return data
