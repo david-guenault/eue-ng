3de89130ef98273d5e7a9e7d5d47438b5b2d96b0
David GUENAULT <dguenault@monitoring-fr.org>
2013-09-07 14:33:04 +0200

    Added helpers for checking email validity and provided fields

--- lib/eueauth.py
+++ lib/eueauth.py
@@ -1,11 +1,39 @@
 #!/usr/bin/python
 # -*- coding: <encoding name> -*-
 
+import re
+
 
 class auth:
 
     def __init__(self):
         return
+
+    def check_fields(self, user=None, password=None):
+        """
+        check if fields are provided and are not empty
+        """
+
+        if not user or not password:
+            return False
+
+        if len(user) == 0 or len(password) == 0:
+            return False
+
+        return True
+
+    def check_mail(self, email=None):
+        """
+        Verify that the provided email is valid
+        """
+
+        regex = r"[^@]+@[^@]+\.[^@]+"
+        if not re.match(regex, email):
+            return False
+        else:
+            return True
+
+        return True
 
     def check_auth(self, user, password):
         """
