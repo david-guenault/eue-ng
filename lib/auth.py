de5d46820c31c7a764785a7862e81df655b64f77
David GUENAULT <dguenault@monitoring-fr.org>
2013-09-07 10:41:22 +0200

    Added authentication class skeleton

---
+++ lib/auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+
+
+class myAuth:
+
+    def __init__(self):
+        return True
+
+    def check_auth(self, user, password):
+        """
+        Verify authentication credentials
+        """
+        return True
