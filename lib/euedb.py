b61ded5a1f59eca7838219d9e904941bd04aa064
David GUENAULT <dguenault@monitoring-fr.org>
2013-09-07 11:49:17 +0200

    Add better connection management ...

--- lib/euedb.py
+++ lib/euedb.py
@@ -18,6 +18,8 @@ class mysql:
     user = None
     passw = None
 
+    connected = False
+
     def __init__(self, host, user, passw, db, port=3306):
         self.port = port
         self.host = host
@@ -25,24 +27,33 @@ class mysql:
         self.passw = passw
         self.db = db
         if not self.connect():
-            return False
+            self.connected = False
 
     def connect(self):
         """
         connect to the mysql db
         """
         try:
-            self.cn = MySQLdb.connect(
-                host=self.host, user=self.user, passwd=self.passw,
-                db=self.db, port=self.port)
-            return True
+            if self.connected:
+                return True
+            else:
+                self.cn = MySQLdb.connect(
+                    host=self.host, user=self.user, passwd=self.passw,
+                    db=self.db, port=self.port)
+                self.connected = True
+                return True
         except:
+            self.connected = False
             return False
 
     def querySelect(self, query):
         """
         do the select query and return the result as an array of dictionaries
         """
+
+        if not self.connected:
+            self.connect()
+
         data = []
         cur = self.cn.cursor(cursors.DictCursor)
         try:
