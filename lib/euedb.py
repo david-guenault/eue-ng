62a4860590b967838d4da4a90bbd2b17ece716b1
David GUENAULT <dguenault@monitoring-fr.org>
2013-09-08 09:11:59 +0200

    removed unused code and comments

--- lib/euedb.py
+++ lib/euedb.py
@@ -7,65 +7,6 @@ import time
 import re
 from pymongo import MongoClient
 
-# class mysql:
-
-#     import MySQLdb
-#     from MySQLdb import cursors
-
-#     cn = None
-
-#     host = None
-#     port = None
-#     user = None
-#     passw = None
-
-#     connected = False
-
-#     def __init__(self, host, user, passw, db, port=3306):
-#         self.port = port
-#         self.host = host
-#         self.user = user
-#         self.passw = passw
-#         self.db = db
-#         if not self.connect():
-#             self.connected = False
-
-#     def connect(self):
-#         """
-#         connect to the mysql db
-#         """
-#         try:
-#             if self.connected:
-#                 return True
-#             else:
-#                 self.cn = MySQLdb.connect(
-#                     host=self.host, user=self.user, passwd=self.passw,
-#                     db=self.db, port=self.port)
-#                 self.connected = True
-#                 return True
-#         except:
-#             self.connected = False
-#             return False
-
-#     def querySelect(self, query):
-#         """
-#         do the select query and return the result as an array of dictionaries
-#         """
-
-#         if not self.connected:
-#             self.connect()
-
-#         data = []
-#         cur = self.cn.cursor(cursors.DictCursor)
-#         try:
-#             cur.execute(query)
-#         except:
-#             return False
-
-#         data = cur.fetchall()
-#         cur.close()
-#         return data
-
 
 class mongo:
 
