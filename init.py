5110333263f85269b00d6ca5a6707abe98ac7a50
David GUENAULT <david.guenault@gmail.com>
2013-09-13 16:42:42 +0200

    Added user collection init

---
+++ init.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from lib import eueuser
+
+if __name__ == '__main__':
+    from lib import euemongo
+    m = euemongo.mongo(host="localhost", port=27017, database="eue")
+    m.connect()
+    u = eueuser.user(m, "users")
+    print u.delete("david.guenault@gmail.com")
+    print u.new({"email": "david.guenault@gmail.com", "password": "dfgdfg"})
+    print m.disconnect()
